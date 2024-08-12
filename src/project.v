f3c518f78fe462f878a525320b3106c85e11f62a
mole99 <leomoser99@gmail.com>
2024-08-12 10:22:40 +0200

    Rename Verilog module

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_um_example (
+module tt_um_tt08_aicd_playground (
     input  wire       VGND,
     input  wire       VDPWR,    // 1.8v power supply
 //    input  wire       VAPWR,    // 3.3v power supply
